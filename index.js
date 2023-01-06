c325c5d65f831c459628a8ec738dfcdc8983c2e9
Mike Casto <castoware@gmail.com>
2023-01-06 16:54:54 -0500

    Deploy for basic testing

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+const test = require("./test.js");
+test();
+
+var http = require("http");
+http
+  .createServer(function (req, res) {
+    console.log(`Just got a request at ${req.url}!`);
+    res.write("Yo!");
     res.end();
-}).listen(process.env.PORT || 3000);
+  })
+  .listen(process.env.PORT || 3000);
---
+++ temp.js
@@ -0,0 +1,59 @@
+/*
+    $server = 'imap.titan.email';
+    $port = '993';
+    $username = "cuenca-writers-collective-rsvp@castoware.com";
+    $password = ",3sBwo?7^qab>oE40P}J";
+*/
+
+const { ImapFlow } = require("imapflow");
+const simpleParser = require("mailparser").simpleParser;
+
+const settings = { y: "Yes", n: "No", t: "Tentative" };
+
+const client = new ImapFlow({
+  host: "imap.titan.email",
+  port: 993,
+  secure: true,
+  auth: {
+    user: "cuenca-writers-collective-rsvp@castoware.com",
+    pass: ",3sBwo?7^qab>oE40P}J",
+  },
+  emitLogs: false,
+  logger: false,
+});
+
+const main = async () => {
+  // Wait until client connects and authorizes
+  await client.connect();
+
+  // Select and lock a mailbox. Throws if mailbox does not exist
+  let lock = await client.getMailboxLock("INBOX");
+
+  try {
+    for await (let message of client.fetch("1:*", {
+      source: true,
+      envelope: true,
+    })) {
+      const src = await simpleParser(message.source);
+      const date = message.envelope.date;
+      const from = message.envelope.from.shift().address;
+      const body = src.text;
+      let response = body.match(
+        /^(?<attending>yes|no|tentative)\s*(?<reading>reading)?/i
+      );
+
+      rsvp = response ? response.groups : { attending: null, reading: null };
+
+      console.log({ date, from, body, rsvp });
+
+      // mc-todo: update rec in DB with rsvp info
+    }
+  } finally {
+    lock.release();
+  }
+
+  // log out and close connection
+  await client.logout();
+};
+
+main().catch((err) => console.error(err));
