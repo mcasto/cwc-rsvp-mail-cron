964d59394da2f34dbf9d0bb103ddfdea45b97264
Mike Casto <castoware@gmail.com>
2023-01-06 17:03:50 -0500

    test6

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var http = require("http");
 http
   .createServer(async function (req, res) {
     const mail = await handleImap();
-    res.write(mail);
+    res.write(JSON.stringify(mail));
     res.end();
   })
   .listen(process.env.PORT || 3000);
